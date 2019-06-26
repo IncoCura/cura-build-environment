bc90b816985b86c07b848bc55e22524263fc5a9c
KangDroid <hyunwoo286@gmail.com>
2019-06-26 21:44:46 +0900

    Track my OpenBLAS Again

--- projects/openblas.cmake
+++ projects/openblas.cmake
@@ -4,8 +4,8 @@ if(NOT BUILD_OS_WINDOWS)
     set(openblas_options DYNAMIC_ARCH=1 NO_STATIC=1)
 
     ExternalProject_Add(OpenBLAS
-        URL https://github.com/xianyi/OpenBLAS/archive/v0.3.5.tar.gz
-        URL_MD5 579bda57f68ea6e9074bf5780e8620bb
+        GIT_REPOSITORY https://github.com/IncoCura/OpenBLAS
+        GIT_TAG origin/develop
         CONFIGURE_COMMAND ""
         BUILD_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options}
         INSTALL_COMMAND make PREFIX=${CMAKE_INSTALL_PREFIX} ${openblas_options} install
